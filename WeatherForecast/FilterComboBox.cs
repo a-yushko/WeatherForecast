931ee8ac170edf16818c9de2a3891fed2fac69a0
ayushko <ayushko@serena.com>
2017-06-02 11:14:02 +0300

    Custom combobox control

---
+++ WeatherForecast/FilterComboBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WeatherForecast
+{
+    public class FilterComboBox : ComboBox
+    {
+        private SelectionChangedEventArgs _lastSelectionChangedArgs;
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            if (SelectedIndex != -1)
+                _lastSelectionChangedArgs = e;
+        }
+
+        protected override void OnDropDownClosed(EventArgs e)
+        {
+            if (_lastSelectionChangedArgs != null)
+                OnSelectionChanged(_lastSelectionChangedArgs);
+            base.OnDropDownClosed(e);
+        }
+    }
+}
